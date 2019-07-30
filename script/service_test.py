df3c02fea152f3d4f4638bdb2ca856f4e4dc5a89
Daniil <m1802288@edu.misis.ru>
2019-07-30 17:29:23 +0100

    V3

--- script/service_test.py
+++ script/service_test.py
@@ -5,7 +5,7 @@ import time
 from wheelchair.srv import *
 
 if __name__ == '__main__':
-
+    node = rospy.init_node('test_node')
     print_service = rospy.ServiceProxy('/Print_service', batLevel)
     request = PrintRequest()
     request.message.data = ""
