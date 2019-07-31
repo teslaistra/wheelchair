11844834998cd41ab070ff4df125ca287e9f3316
Daniil <m1802288@edu.misis.ru>
2019-07-31 16:18:36 +0100

    V3

--- script/can2RNET.py
+++ script/can2RNET.py
@@ -95,5 +95,5 @@ def cansend(s,cansendtxt):
 def opencansocket(busnum):
     busnum='can'+str(busnum)
     bus = can.interface.Bus(channel = busnum, bustype='socketcan')
-    print ("connected to CAN"+busnum)
+    print ("connected to "+busnum)
     return bus
