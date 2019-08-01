5f0049ab26f40e38f5e1da241dab1f89af05da9e
Daniil <m1802288@edu.misis.ru>
2019-08-01 11:08:58 +0100

    V3

--- script/ArrayReading.py
+++ script/ArrayReading.py
@@ -6,10 +6,35 @@ from fcntl import ioctl
 import select
 import threading
 from can2RNET import *
-
 import binascii
-from reader_node import periodic_messages, drive_control, event_messages, dec2hex
-from pi_node import induce_JSM_error
+event_messages = {
+ '181c0000': 'song',
+ '0c180201': '0c180201 seen after change mode to angle 1',
+ '00000062': 'chairAngle motor: 40 -running, 00 -stopped',
+ '00000060': '00000060 seen after change mode to angle 3',
+ '00000061': '00000061 seen after change mode to angle 4',
+ '0c180200': '0c180200 seen after change mode to angle 5',
+ '0c180101': '0c180101 seen after change mode to drive 6',
+ '0c180100': '0c180100 seen after change mode to drive 7',
+ '0c000003': 'Blinking button(on the left of panel)',
+ '181c0200': 'song',
+ '1c240001': 'Device is ready. UI is active.',
+ '0c000200': 'unknown',
+ '0c000004': 'Lightning Button',
+ '0c040000': 'horn on',
+ '0c040001': 'horn off',
+ '0c000002': 'right turn button',
+ '0c000001': 'left turn button',
+ '00000051': 'seen when speed changing',
+ '00000050': 'seen when speed changing',
+ '14300101': 'PMtx angle/height motor current',
+ '00000000': 'seen at power off',
+ '00000002': 'seen at power off',
+ '0000000c': 'used by JSM to check for canbus connection',
+ '00000004': 'seen at power off',
+ '0c000105': 'motor has stopped',
+ '0c000106': 'motor is decelerating'
+}
 
 bus = opencansocket(0)
 
